586d20b698b72e82a72fbb95f0993ede0c0be7d9
xian <xianwu2001@yahoo.com>
2024-11-30 12:23:50 -0700

    test with variation voltage, results slightly change arround the voltage

--- ADC-test/main.c
+++ ADC-test/main.c
@@ -1,44 +1,38 @@
 #include <msp430.h>
 
-void configureADC(void);
-unsigned int readADC(unsigned int channel);
-
-int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
-    PM5CTL0 &= ~LOCKLPM5;       // Disable GPIO high-impedance mode
-
-    // Configure ADC
-    configureADC();
-
-    unsigned int p1_2_raw;
-    volatile float p1_2_voltage;
-
-    while (1) {
-        // Read voltage on P1.2
-        p1_2_raw = readADC(ADC12INCH_2); // P1.2 (Channel 2)
-        p1_2_voltage = (p1_2_raw * 3.3) / 4096; // Assuming Vcc = 3.3V
-
-        __no_operation(); // Breakpoint here to monitor `p1_2_voltage` in the debugger
-    }
-}
-
-void configureADC(void) {
+void configureADCForPin(void) {
     // Configure P1.2 as ADC input
-    P1SEL1 |= BIT2;    // Enable analog function on P1.2
+    P1SEL1 |= BIT2;  // Set P1.2 as analog input
     P1SEL0 |= BIT2;
 
-    // Configure ADC12 module
-    ADC12CTL0 = ADC12SHT0_2 | ADC12ON;    // Sampling time, ADC on
-    ADC12CTL1 = ADC12SHP;                 // Use sampling timer
-    ADC12CTL2 = ADC12RES_2;               // 12-bit resolution
-    ADC12MCTL0 = ADC12VRSEL_0 | ADC12INCH_2; // Use Vcc as reference, Channel 2
-    ADC12CTL0 |= ADC12ENC;                // Enable ADC conversions
+    // Configure ADC12_B
+    ADC12CTL0 = ADC12SHT0_3 | ADC12ON;       // 32 ADC clock cycles sample time, ADC ON
+    ADC12CTL1 = ADC12SHP;                    // Use sampling timer
+    ADC12CTL2 = ADC12RES_2;                  // 12-bit resolution
+    ADC12MCTL0 = ADC12INCH_2;                // Input channel A2 (P1.2), VREF = Vcc
+    ADC12CTL0 |= ADC12ENC;                   // Enable ADC conversions
 }
 
-unsigned int readADC(unsigned int channel) {
-    ADC12MCTL0 = ADC12VRSEL_0 | channel;  // Use Vcc as reference, select channel
-    ADC12CTL0 |= ADC12SC;                 // Start conversion
-    while (!(ADC12IFGR0 & ADC12IFG0));    // Wait for conversion to complete
-    return ADC12MEM0;                     // Return ADC value
+unsigned int readADCValue(void) {
+    ADC12CTL0 |= ADC12SC;                // Start conversion
+    while (!(ADC12IFGR0 & BIT0));        // Wait for conversion to complete
+    return ADC12MEM0;                    // Return ADC value
 }
 
+float convertADCToVoltage(unsigned int adcValue) {
+    return (adcValue * 3.3) / 4095.0;    // Convert ADC value to voltage using Vcc = 3.3V
+}
+
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;            // Stop watchdog timer
+
+    configureADCForPin();                // Configure ADC for P1.2 (A2)
+
+    while (1) {
+        volatile unsigned int adcValue = readADCValue(); // Read ADC value directly
+        volatile float voltage = convertADCToVoltage(adcValue); // Convert to voltage
+
+        // Debugging: Place a breakpoint here to inspect `adcValue` and `voltage`
+        __no_operation();
+    }
+}
